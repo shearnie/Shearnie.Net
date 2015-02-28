6baca1d85efe2afb09d53f33c501e76eabfdde42
Steve Shearn <steve@syntronian.com>
2015-02-28 12:10:04 +1000

    datetime shortcuts for AEST zone

---
+++ source/Shearnie.Net/OzTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shearnie.Net
+{
+    public class OzTime
+    {
+        public static TimeZoneInfo tzidAEST
+        {
+            get
+            {
+                //// Get timezone id's here: http://stackoverflow.com/questions/5996320/net-timezoneinfo-from-olson-time-zone
+                return TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
+            }
+        }
+
+        public static DateTime ConvertUTC_To_AEST(DateTime date)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(date, tzidAEST);
+        }
+
+        public static DateTime GetNowAEST()
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzidAEST);
+        }
+
+        public static DateTime GetMidnightAEST()
+        {
+            return GetNowAEST().Date;
+        }
+
+        public static DateTime GetMidnightUTC()
+        {
+            return TimeZoneInfo.ConvertTimeToUtc(GetMidnightAEST(), tzidAEST);
+        }
+    }
+}
