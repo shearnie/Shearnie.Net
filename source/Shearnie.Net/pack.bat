d4306c13917f3a7018b54b59049338a851a64dd4
Steve Shearn <steve@syntronian.com>
2015-02-28 19:59:38 +1000

    push to nuget 1.1

--- source/Shearnie.Net/pack.bat
+++ source/Shearnie.Net/pack.bat
@@ -1,2 +1,2 @@
 ..\.nuget\nuget pack Shearnie.Net.csproj -IncludeReferencedProjects -Prop Configuration=Release
-..\.nuget\nuget push Shearnie.Net.1.0.nupkg
+..\.nuget\nuget push Shearnie.Net.1.1.nupkg
