909723598aa7e3c167053ddbaf995e2c08738bb4
Steve Shearn <steve@syntronian.com>
2015-10-03 23:53:07 +1000

    v 1.6

--- source/Shearnie.Net/pack.bat
+++ source/Shearnie.Net/pack.bat
@@ -1,2 +1,2 @@
 ..\.nuget\nuget pack Shearnie.Net.csproj -IncludeReferencedProjects -Prop Configuration=Release
-..\.nuget\nuget push Shearnie.Net.1.5.nupkg
+..\.nuget\nuget push Shearnie.Net.1.6.nupkg
