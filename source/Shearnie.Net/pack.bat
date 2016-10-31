d8b14a052a34e9cbc1d75e3b76fb42d7f53bc53a
Steve Shearn <steve@syntronian.com>
2016-10-31 13:59:40 +1000

    v1.10

--- source/Shearnie.Net/pack.bat
+++ source/Shearnie.Net/pack.bat
@@ -1,6 +1,6 @@
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ../Shearnie.Net.sln /rebuild Release
 
 ..\.nuget\nuget pack Shearnie.Net.csproj -IncludeReferencedProjects -Prop Configuration=Release
-..\.nuget\nuget push Shearnie.Net.1.9.nupkg
+..\.nuget\nuget push Shearnie.Net.1.10.nupkg
 
 pause
